83cefd9131f4bfd59f605fbbc4934c804ca4bdba
Brooks <ubuntu@ip-172-31-1-64.ec2.internal>
2022-08-02 14:13:25 +0000

    removing comments

--- modules/aws_data/outputs.tf
+++ modules/aws_data/outputs.tf
@@ -1,11 +1,7 @@
 output "random_number" {
   value = random_integer.rando-id
 }
-/*
-output "iam-user" {
-  value = data.aws_iam_user.s3_user
-}
-*/
+
 output "petname" {
   value = random_pet.test
 }
