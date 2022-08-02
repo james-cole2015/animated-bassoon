09a3f00ef0bc9b8e968e93548e05f5b76253177c
Ubuntu <ubuntu@ip-172-31-90-143.ec2.internal>
2022-08-02 21:01:13 +0000

    changing s3 backend

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "cautious-broccoli-backend-133909"
+    bucket         = "amazing-waterfall-13739"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "cautious-broccoli-dynamodbtable"
+    dynamodb_table = "amazing-waterfall-table"
   }
   required_providers {
     aws = {
@@ -19,5 +19,4 @@ provider "aws" {
   region                   = var.region
   shared_credentials_files = ["~/.aws/credentials"]
   shared_config_files      = ["~/.aws/config"]
-  profile                  = "default"
 }
