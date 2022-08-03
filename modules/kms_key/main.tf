864eab587a02127a813ea4e573c86f5296ac313c
Brooks <ubuntu@ip-172-31-90-143.ec2.internal>
2022-08-03 15:15:56 +0000

    adding kms key configuration

---
+++ modules/kms_key/main.tf
@@ -0,0 +1,11 @@
+resource "aws_kms_key" "kms-key" {
+    description = "KMS Key for EKS"
+    key_usage = "ENCRYPT_DECRYPT"
+
+    tags = {
+        platform = "terraform" 
+        environment = "development" 
+        repo-name = "${var.repo-name}"
+        function = "encryption_key"
+    }
+}
