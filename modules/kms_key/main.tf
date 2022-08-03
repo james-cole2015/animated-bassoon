68e42ac351adfb9ffb5cb92fc5de2c7418434f1f
Brooks <ubuntu@ip-172-31-90-143.ec2.internal>
2022-08-03 15:42:21 +0000

    adding name for kms key

--- modules/kms_key/main.tf
+++ modules/kms_key/main.tf
@@ -1,11 +1,12 @@
 resource "aws_kms_key" "kms-key" {
-    description = "KMS Key for EKS"
-    key_usage = "ENCRYPT_DECRYPT"
+  description = "KMS Key for EKS"
+  key_usage   = "ENCRYPT_DECRYPT"
 
-    tags = {
-        platform = "terraform" 
-        environment = "development" 
-        repo-name = "${var.repo-name}"
-        function = "encryption_key"
-    }
+  tags = {
+    platform    = "terraform"
+    environment = "development"
+    repo-name   = "${var.repo-name}"
+    function    = "encryption_key"
+    Name        = "EKS-Key"
+  }
 }
