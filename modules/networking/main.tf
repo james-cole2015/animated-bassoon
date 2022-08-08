042d528f6d4a54dc4984f0994a9d14aad36f9fbf
Brooks <ubuntu@ip-172-31-90-143.ec2.internal>
2022-08-08 19:21:26 +0000

    removing unused block

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -35,9 +35,9 @@ resource "aws_security_group" "worker_group_mgmt_one" {
     to_port   = 22
     protocol  = "tcp"
 
-    cidr_blocks = var.cidr_block
+    cidr_blocks = [var.cidr_block]
   }
-
+}
   resource "aws_security_group" "all_worker_mgmt" {
     name_prefix = "all_worker_mgmt"
     vpc_id      = module.vpc.vpc_id
@@ -50,4 +50,3 @@ resource "aws_security_group" "worker_group_mgmt_one" {
       cidr_blocks = var.eks-clusters-cidr_blocks
     }
   }
-}
