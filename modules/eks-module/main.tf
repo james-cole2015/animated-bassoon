e9466a1e8ede2624fc25602536213b68bbf9d126
Brooks <ubuntu@ip-172-31-90-143.ec2.internal>
2022-08-08 23:17:24 +0000

    fixing iam_role_arn

--- modules/eks-module/main.tf
+++ modules/eks-module/main.tf
@@ -18,7 +18,7 @@ module "eks" {
     }
   }
   create_iam_role = false
-  iam_role = var.eks_iam_role
+  iam_role_arn = var.eks_iam_role
 
   vpc_id     = var.vpc_id
   subnet_ids = [var.subnet_ids]
