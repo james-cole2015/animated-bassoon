5420a8de91ddae3063dee1f9ffe5b51a1d56d6b2
Brooks <ubuntu@ip-172-31-90-143.ec2.internal>
2022-08-09 00:41:10 +0000

    fixing typo

--- modules/k8s-resources/main.tf
+++ modules/k8s-resources/main.tf
@@ -25,11 +25,11 @@ resource "kubernetes_deployment" "demo" {
           name  = "example"
 
           resources {
-            limits {
+            limits = {
               cpu    = "0.5"
               memory = "512Mi"
             }
-            requests {
+            requests = {
               cpu    = "250m"
               memory = "50Mi"
             }
